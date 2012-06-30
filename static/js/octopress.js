864693d85bc9ed472572ee8fdb4fe69f9f573a2c
lepture <lepture@me.com>
2012-07-01 00:06:54 +0800

    almost finished

---
+++ static/js/octopress.js
@@ -0,0 +1,32 @@
+// Source url: https://gist.github.com/901295
+(function(doc) {
+    var addEvent = 'addEventListener',
+    type = 'gesturestart',
+    qsa = 'querySelectorAll',
+    scales = [1, 1],
+    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+    function fix() {
+        meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+        doc.removeEventListener(type, fix, true);
+    }
+    if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+        fix();
+        scales = [0.25, 1.6];
+        doc[addEvent](type, fix, true);
+    }
+}(document));
+
+(function(d) {
+    var toggle = d.createElement('span');
+    toggle.className = 'toggle-sidebar';
+    var content = d.getElementById('content');
+    content.appendChild(toggle);
+    toggle.onclick = function(e) {
+        e.preventDefault();
+        if (d.body.className == 'collapse-sidebar') {
+            d.body.className = '';
+        } else {
+            d.body.className = 'collapse-sidebar';
+        }
+    };
+})(document);
